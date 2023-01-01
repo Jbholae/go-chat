b112366e51b1b27c1f42969dac18f5b97e1fecf4
saurav <saurav.maharjan7@gmail.com>
2023-01-02 00:00:47 +0545

    added created,updated,deleted fields

--- models/room.go
+++ models/room.go
@@ -1,12 +1,22 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type Room struct {
-	Id      string
-	Name    string
-	Private bool
+	gorm.Model
+	Id        int64
+	Name      string
+	Private   bool
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
 
-func (room *Room) GetId() string {
+func (room *Room) GetId() int64 {
 	return room.Id
 }
 
--- models/user.go
+++ models/user.go
@@ -1,8 +1,19 @@
 package models
 
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
 type User struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
+	gorm.Model
+	Id        string         `json:"id" gorm:"primary_key"`
+	Name      string         `json:"name"`
+	Email     string         `json:"email"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 }
 
 func (user *User) GetId() string   { return user.Id }
